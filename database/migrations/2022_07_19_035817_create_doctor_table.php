e04f10f6da73403a16df8c264234e4ab62663a86
HAM23HAM <63126492+HAM23HAM@users.noreply.github.com>
2022-07-19 13:24:20 +0700

    migrate table

--- database/migrations/2022_07_19_035817_create_doctor_table.php
+++ database/migrations/2022_07_19_035817_create_doctor_table.php
@@ -13,16 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('doctor', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
+            $table->integer('specialist_id');
+            $table->integer('user_id');
+            $table->integer('name');
+            $table->integer('fee');
+            $table->integer('photo')->nullable();
             $table->softDeletes();
         });
     }
@@ -34,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('doctor');
     }
 };
